3bfdee5e8ccb24ebd0f90d15da7a08109403bb69
your name here <ahrampy@gmail.com>
2020-02-24 18:48:07 -0800

    add vue config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// module.exports = {
+//   devServer: {
+//     hot: false,
+//     liveReload: true
+//   }
+// };
 
 new Vue({
   render: h => h(App),
